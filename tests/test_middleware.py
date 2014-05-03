645c75659177f5b56d29cb22abef19a664ede12f
Christian Schwede <christian.schwede@enovance.com>
2014-05-03 10:08:58 +0000

    Sample 04: testing our middleware
    You need to install WebOb first:
    
        sudo pip install WebOb
    
    Run tests with
    
        python tests/test_middleware.py

---
+++ tests/test_middleware.py
@@ -0,0 +1,25 @@
+import StringIO
+import unittest
+
+from webob import Request
+
+from sample.middleware import SummitMiddleware
+
+
+class FakeApp(object):
+    def __call__(self, env, start_response):
+        start_response('200 OK', [])
+        return ""
+
+
+class TestSummitMiddleware(unittest.TestCase):
+    def test_simple_request(self):
+        environ={'REQUEST_METHOD': 'PUT'}
+        req = Request.blank('/echo', environ, body='Hello World')
+        mw = SummitMiddleware(FakeApp())
+        response = req.get_response(mw)
+        self.assertEqual("Hello World", response.body)
+
+
+if __name__ == '__main__':
+    unittest.main()
