0a62393334d1ba411c06995b51a3e03af9c8443e
Christian Schwede <christian.schwede@enovance.com>
2014-05-03 09:01:41 +0000

    Sample 02: return some useful informations

--- sample/app.py
+++ sample/app.py
@@ -2,6 +2,10 @@ from wsgiref.simple_server import make_server
 
 def myapp(environ, start_response):
     body = []
+    keys = ['PATH_INFO','QUERY_STRING', 'REQUEST_METHOD', 'HTTP_HEADERNAME', 'wsgi.input']
+    for key in keys:
+        value = environ.get(key)
+        body.append("%s: '%s'\n" % (key, value))
     headers = [('Content-Type', 'text/plain')]
     start_response('200 OK', headers)
     return body
