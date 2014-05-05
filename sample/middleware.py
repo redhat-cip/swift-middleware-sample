808c08fffe6d019471110305161778768ed2c4b7
Christian Schwede <christian.schwede@enovance.com>
2014-05-05 08:13:13 +0000

    Sample 07: use wsgify

--- sample/middleware.py
+++ sample/middleware.py
@@ -1,14 +1,16 @@
+from swift.common.swob import HTTPOk, wsgify
+
+
 class SummitMiddleware(object):
     def __init__(self, app, *args, **kwargs):
         self.app = app
         self.suffix = kwargs.get('suffix', '')
 
-    def __call__(self, env, start_response):
-        response = self.app(env, start_response)
-        if env.get('PATH_INFO') == self.suffix:
-            length = int(env.get('CONTENT_LENGTH') or 0)
-            return env.get('wsgi.input').read(length)
-        return response
+    @wsgify
+    def __call__(self, request):
+        if request.path_info == self.suffix:
+            return HTTPOk(request=request, body=request.body)
+        return self.app
 
 def filter_factory(global_config, **local_config):
     suffix = local_config.get('suffix')
